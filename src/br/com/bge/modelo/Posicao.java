f506b84d337943698cefb8d62f83c954c9a8961f
Nicolas Araujo <niaraujo0807@gmail.com>
2026-09-01 21:03:52 -0300

    feat: Adiciona classe de dominio Posicao imutavel com equals e hashCode

---
+++ src/br/com/bge/modelo/Posicao.java
@@ -0,0 +1,36 @@
+package br.com.bge.modelo;
+
+import java.util.Objects;
+
+public class Posicao {
+	
+	private final int linha;
+	private final int coluna;
+	
+	public Posicao (int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+	
+	public int getColuna() {
+		return coluna;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Posicao posicao = (Posicao) o;
+		return linha == posicao.linha && coluna == posicao.coluna;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(linha, coluna);
+	}
+	
+}
