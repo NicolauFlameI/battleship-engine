ab901e7e902ab5e4c4841ffdb0739cf4f42ed157
Nicolas Araujo <niaraujo0807@gmail.com>
2026-09-01 21:23:58 -0300

    feat: Adiciona classe Navio com regras de negocio salvas

--- src/br/com/bge/modelo/Navio.java
+++ src/br/com/bge/modelo/Navio.java
@@ -1,5 +1,54 @@
 package br.com.bge.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Navio {
+	
+	private final String nome;
+	private final int tamanho;
+	
+	private final List<Posicao> posicoesOcupadas;
+	
+	private int acertosRecebidos;
+	
+	public Navio(String nome, int tamanho) {
+		this.nome = nome;
+		this.tamanho = tamanho;
+		this.posicoesOcupadas = new ArrayList<>();
+		this.acertosRecebidos = 0;
+	}
+	
+	public void adicionarPosicao(Posicao posicao) {
+		if (posicoesOcupadas.size() < this.tamanho){
+			posicoesOcupadas.add(posicao);
+		}
+	}
+	
+	public boolean ocupaPosicao(Posicao posicaoDisparada) {
+		return this.posicoesOcupadas.contains(posicaoDisparada);
+	}
+	
+	public void registrarAcerto() {
+		if (!estaAfundado()) {
+			this.acertosRecebidos++;
+		}
+	}
+	
+	public boolean estaAfundado() {
+		return this.acertosRecebidos >= this.tamanho;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+	
+	public List<Posicao> getPosicoesOcupadas(){
+		return new ArrayList<>(this.posicoesOcupadas);
+	}
 
 }
