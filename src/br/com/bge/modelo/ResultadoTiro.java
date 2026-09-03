58db86546094355b33d8bf6e6003b2444b71bc8b
Nicolas Araujo <niaraujo0807@gmail.com>
2026-09-02 21:47:02 -0300

    feat: Adiciona DTO ResultadoTiro para empacotar retorno de disparos

--- src/br/com/bge/modelo/ResultadoTiro.java
+++ src/br/com/bge/modelo/ResultadoTiro.java
@@ -1,5 +1,44 @@
 package br.com.bge.modelo;
 
 public class ResultadoTiro {
+	
+	private final boolean acertou;
+	private final boolean afundouNavio;
+	private final String nomeNavioAtingido;
+	private final int naviosRestantes;
+	private final String mensagem;
+	
+	public ResultadoTiro(boolean acertou, 
+			boolean afundouNavio, 
+			String nomeNavioAtingido, 
+			int naviosRestantes,
+			String mensagem) {
+		this.acertou = acertou;
+		this.afundouNavio = afundouNavio;
+		this.nomeNavioAtingido = nomeNavioAtingido;
+		this.naviosRestantes = naviosRestantes;
+		this.mensagem = mensagem;
+	}
+
+	public boolean isAcertou() {
+		return acertou;
+	}
+
+	public boolean isAfundouNavio() {
+		return afundouNavio;
+	}
+
+	public String getNomeNavioAtingido() {
+		return nomeNavioAtingido;
+	}
+
+	public int getNaviosRestantes() {
+		return naviosRestantes;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+	
 
 }
